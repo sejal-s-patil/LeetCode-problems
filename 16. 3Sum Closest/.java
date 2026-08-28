b851ca8e9d12be991475bd56943185a884e27661
Sejal Patil <sejalpatil01112006@gmail.com>
2026-08-28 22:35:29 +0530

    Create .java

---
+++ 16. 3Sum Closest/.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        int closest=nums[0]+nums[1]+nums[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                    closest = sum;
+                }
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+
+        return closest;
+    }
+}
